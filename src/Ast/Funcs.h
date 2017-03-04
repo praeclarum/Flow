2f71ff543a3afee1b0846e30a7142d4881154eea
Frank A. Krueger <fak@praeclarum.org>
2017-03-04 02:08:34 -0800

    Define the AST

---
+++ src/Ast/Funcs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Ast.h"
+
+extern const Func *addFunc;
+extern const Func *subFunc;
+extern const Func *mulFunc;
+extern const Func *divFunc;
+extern const Func *negFunc;
+extern const Func *powFunc;
