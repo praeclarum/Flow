a64045ff630393740b36974e75c3fc5b2537d970
Frank A. Krueger <fak@praeclarum.org>
2017-03-16 20:46:24 -0700

    Serve JSON

--- src/WebServer/WebServer.cpp
+++ src/WebServer/WebServer.cpp
@@ -26,17 +26,23 @@ void WebServer::loop()
     WiFiClient client = server.available();
     if (client) {
         bool currentLineIsBlank = true;
+        String firstLine;
+        bool gotFirstLine = false;
         while (client.connected()) {
             if (client.available()) {
                 char c = client.read();
                 if (c == '\n' && currentLineIsBlank) {
-                    sendReply("", client);
+                    sendReply(firstLine.c_str(), client);
                     break;
                 }
                 if (c == '\n') {
                     currentLineIsBlank = true;
+                    gotFirstLine = true;
                 } else if (c != '\r') {
                     currentLineIsBlank = false;
+                    if (!gotFirstLine) {
+                        firstLine += (char)c;
+                    }
                 }
             }
         }
@@ -47,6 +53,7 @@ void WebServer::loop()
 
 static void printNode(Node *node, WiFiClient &client)
 {
+    client.print(F("{\"nodeType\":\""));
     switch (node->nodeType)
     {
     case NT_Null:
@@ -92,83 +99,92 @@ static void printNode(Node *node, WiFiClient &client)
         client.print(F("End"));
         break;        
     }
-    client.print(F(" "));
-    client.print((uintptr_t)node, HEX);
-    client.print(F(" "));
+    client.print(F("\",\"value\":"));
     switch (node->nodeType)
     {
     case NT_Null:
-        break;
-    case NT_Document:
+        client.print(F("null"));
         break;
     case NT_Number:
         client.print(node->value.number);
         break;
     case NT_BinaryOperator:
+        client.print('"');
         client.print((char)node->value.binaryOperator);
+        client.print('"');
         break;
     case NT_UnaryOperator:
+        client.print('"');
         client.print((char)node->value.unaryOperator);
-        break;
-    case NT_Call:
-        break;
-    case NT_Function:
-        client.print((uintptr_t)node->value.function, HEX);
-        break;
-    case NT_Sub:
+        client.print('"');
         break;
     case NT_Assignment:
         client.print(node->value.number);
         break;
     case NT_Name:
+        client.print('"');
         client.print(node->value.name, HEX);
+        client.print('"');
         break;
     case NT_AssignmentReference:
-        client.print((uintptr_t)node->value.assignmentReference, HEX);
-        break;
     case NT_FunctionReference:
-        client.print((uintptr_t)node->value.functionReference->function, HEX);
-        break;
     case NT_SwitchToSub:
-        client.print(F("SwitchToSub"));
-        break;
+    case NT_Sub:
     case NT_End:
-        client.print(F("End"));
-        break;        
+    case NT_Document:
+    case NT_Call:
+    case NT_Function:
+        client.print('0');
+        break;
     }
+    client.print(F(",\"childNodes\":["));
     Node *c = node->firstChild;
     if (c) {
-        client.println(F("<ul>"));
+        char head=' ';
         while (c) {
-            client.println(F("<li>"));
+            client.print(head);
             printNode(c, client);
-            client.println(F("</li>"));
+            head = ',';
             c = c->nextSibling;
         }
-        client.println(F("</ul>"));
     }
+    client.print(F("]}"));
 }
 
 void WebServer::sendReply(const char *url, WiFiClient &client)
 {
-    // send a standard http response header
-    client.println(F("HTTP/1.1 200 OK"));
-    client.println(F("Content-Type: text/html"));
-    client.println(F("Connection: close"));  // the connection will be closed after completion of the response
-    client.println();
-    client.println(F("<!DOCTYPE html>"));
-    client.println(F("<html>"));
-    client.println(F("<head>"));
-    client.println(F("<link href=\"http://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">"));
-    client.println(F("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css\" media=\"screen,projection\"/>"));
-    client.println(F("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>"));
-    client.println(F("</head><body>"));
-    client.println(F("<div id=\"device\"></div>"));
-    client.println(F("<script src=\"https://unpkg.com/react@15/dist/react.js\"></script>"));
-    client.println(F("<script src=\"https://unpkg.com/react-dom@15/dist/react-dom.js\"></script>"));
-    client.println(F("<script src=\"https://code.jquery.com/jquery-2.1.1.min.js\"></script>"));
-    client.println(F("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js\"></script>"));
-    client.println(F("<script src=\"https://praeclarum.github.io/Flow/device.js\"></script>"));
-    client.println(F("</body></html>"));
+    if (strstr(url, "GET / ") == url) {
+        // send a standard http response header
+        client.println(F("HTTP/1.1 200 OK"));
+        client.println(F("Content-Type: text/html"));
+        client.println(F("Connection: close"));  // the connection will be closed after completion of the response
+        client.println();
+        client.println(F("<!DOCTYPE html>"));
+        client.println(F("<html>"));
+        client.println(F("<head>"));
+        client.println(F("<link href=\"http://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">"));
+        client.println(F("<link type=\"text/css\" rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css\" media=\"screen,projection\"/>"));
+        client.println(F("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>"));
+        client.println(F("</head><body>"));
+        client.println(F("<div id=\"device\"></div>"));
+        client.println(F("<script src=\"https://unpkg.com/react@15/dist/react.js\"></script>"));
+        client.println(F("<script src=\"https://unpkg.com/react-dom@15/dist/react-dom.js\"></script>"));
+        client.println(F("<script src=\"https://code.jquery.com/jquery-2.1.1.min.js\"></script>"));
+        client.println(F("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js\"></script>"));
+        client.println(F("<script src=\"https://praeclarum.github.io/Flow/device.js\"></script>"));
+        client.println(F("</body></html>"));
+    }
+    else if (strstr(url, "GET /document.json ") == url) {
+        client.println(F("HTTP/1.1 200 OK"));
+        client.println(F("Content-Type: application/json"));
+        client.println(F("Connection: close"));  // the connection will be closed after completion of the response
+        client.println();
+        printNode(flow->getDocument(), client);
+    }
+    else {
+        client.println(F("HTTP/1.1 404 Not found"));
+        client.println(F("Connection: close"));  // the connection will be closed after completion of the response
+        client.println();
+    }
 }
 
